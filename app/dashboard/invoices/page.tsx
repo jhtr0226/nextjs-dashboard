e10e907ece84ffa724afd8c1c56c25c5875b7672
jhtr0226 <johanncraft17@gmail.com>
2025-03-23 00:16:44 -0600

    5th

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,23 +1,17 @@
-import { Metadata } from 'next';
-import { Suspense } from 'react';
-
 import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { fetchInvoicesPages } from '@/app/lib/data';
 
-export const metadata: Metadata = {
-  title: 'Invoices',
-};
-
-interface PageProps {
-  searchParams?: Record<string, string | string[] | undefined>;
-}
-
-export default async function Page({ searchParams }: PageProps) {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) {
   const query = typeof searchParams?.query === 'string' ? searchParams.query : '';
   const currentPage =
     typeof searchParams?.page === 'string' ? Number(searchParams.page) : 1;
@@ -29,16 +23,13 @@ export default async function Page({ searchParams }: PageProps) {
       <div className="flex w-full items-center justify-between">
         <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
       </div>
-
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-
       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense>
-
       <div className="mt-5 flex w-full justify-center">
         <Pagination totalPages={totalPages} />
       </div>
